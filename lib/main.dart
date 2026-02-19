aecdbea6bf2be81e73b335d47e7a6b260c67c067
Asur <sahilkulkarni.925@gmail.com>
2026-02-19 17:19:32 +0530

    AGENT TEST

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'core/routing/app_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    getBatteryLevel();
+  }
+
+  Future<int> getBatteryLevel() async {
+    final int result = await const MethodChannel('com.example.app')
+        .invokeMethod('getBatteryLevel');
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
