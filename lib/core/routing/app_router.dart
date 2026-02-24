eff91662600b996c1b52d8322f969e04b0abddfe
Asur <sahilkulkarni.925@gmail.com>
2026-02-24 10:56:32 +0530

    AGENT TEST

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -5,6 +5,7 @@ import '../../features/loan/loan_root_screen.dart';
 
 class AppRouter {
   static Map<String, WidgetBuilder> routes = {
+    '/': (context) => const AccountOpeningScreen(),
     '/kyc': (context) => const KycRootScreen(),
     '/loan': (context) => const LoanRootScreen(),
     // BUG TKT-009: Missing route for video_kyc_screen
